638eb4adb2d1372737313d1c88efe0519e560adb
Amir Arabi <115399531+20Amir04@users.noreply.github.com>
2024-05-22 18:28:01 -0700

    Added background. Authorize has been set for Guide

--- GraduateWork/Pages/Guide.cshtml.cs
+++ GraduateWork/Pages/Guide.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GraduateWork.Pages
 {
+	[Authorize]
 	public class PrivacyModel : PageModel
 	{
 		private readonly ILogger<PrivacyModel> _logger;
--- GraduateWork/Pages/todos/Edit.cshtml.cs
+++ GraduateWork/Pages/todos/Edit.cshtml.cs
@@ -50,8 +50,6 @@ namespace GraduateWork.Pages.todos
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
